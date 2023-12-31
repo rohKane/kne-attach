15059ffa03cdbd061ab179ae86ddb90caf3d66b5
Kane <47999933+rohKane@users.noreply.github.com>
2023-12-31 23:15:17 +0700

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,4 +1,4 @@
---------- ATTACH -------------
+--------- ATTACH Ehh -------------
 local QBCore = exports['qb-core']:GetCoreObject()
 
 local attachedCarModel = "npolvette"
@@ -124,76 +124,3 @@ RegisterCommand("attachncar", function(source, args)
         })
     end
 end)
-
-
-
-
-
-
-
-
-
--------------------- RAPPEL --------------------
--- Register the chat command /droprope
-RegisterCommand('rappelheli', function(source, args, rawCommand)
-    -- Get the player's ped
-    local playerPed = GetPlayerPed(-1)
-
-    -- Check if the player is in a helicopter
-    if IsPedInAnyHeli(playerPed) then
-        -- Make the player rappel from the helicopter
-        TaskRappelFromHeli(playerPed, 0x41200000)
-    else
-        TriggerEvent('chat:addMessage', {
-            color = { 255, 0, 0},
-            multiline = true,
-            args = {"System", "You must be in a helicopter to rappel!"}
-        })
-    end
-end)
-
-
-
---- 2222 ---
-
-local isRappelling = false
-
-RegisterCommand('down', function()
-    local ped = GetPlayerPed(-1)
-    local pos = GetEntityCoords(ped)
-
-    -- Check if the player is near a building
-    if IsNearBuilding(pos) then
-        if not isRappelling then
-            TaskStartScenarioAtPosition(ped, "PROP_HUMAN_SEAT_LEDGE", pos.x, pos.y, pos.z - 1.0, 0.0, 0, 0, 1)
-            isRappelling = true
-        else
-            ClearPedTasks(ped)
-            isRappelling = false
-        end
-    else
-        -- Notify the player that they are not near a suitable building
-        TriggerEvent('chatMessage', 'SYSTEM', {255, 0, 0}, 'You are not near a suitable building to rappel down.')
-    end
-end, false)
-
--- Function to check if the player is near the side of a building
-function IsNearBuilding(position)
-    local ped = GetPlayerPed(-1)
-
-    -- Define the direction for the raycast (downward)
-    local direction = vector3(0.0, 0.0, -1.0)
-
-    -- Perform a raycast from the player's position
-    local rayHandle = StartShapeTestRay(position.x, position.y, position.z + 50.0, position.x, position.y, position.z - 50.0, 10, ped, 0)
-
-    -- Get the result of the raycast
-    local _, hit, _, _, _ = GetShapeTestResult(rayHandle)
-
-    -- Check if the ray hit something and if that something is a building
-    if hit > 0 and IsEntityAPed(hit) == false then
-        return true
-    else
-        return false
-    end
-end
